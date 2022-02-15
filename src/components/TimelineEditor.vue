9239977e783c5da71cf089bedcbd2e4f4a5fb9e2
南门玉 <2781455492@qq.com>
2022-02-15 23:43:40 +0800

    commit - 16

--- src/components/TimelineEditor.vue
+++ src/components/TimelineEditor.vue
@@ -14,17 +14,27 @@
                                 <icon-to-left :style="{ fontSize: '18px' }" />
                             </template>
                         </a-button>
-                        <a-button type="text" size="mini" title="放大视图">
+                        <a-button
+                            @click="controlScaling('enlarge')"
+                            type="text"
+                            size="mini"
+                            title="放大视图"
+                        >
                             <template #icon>
                                 <icon-zoom-in :style="{ fontSize: '18px' }" />
                             </template>
                         </a-button>
-                        <a-button type="text" size="mini" title="缩小视图">
+                        <a-button
+                            @click="controlScaling('reduce')"
+                            type="text"
+                            size="mini"
+                            title="缩小视图"
+                        >
                             <template #icon>
                                 <icon-zoom-out :style="{ fontSize: '18px' }" />
                             </template>
                         </a-button>
-                        <a-button type="text" size="mini" title="后一个事件">
+                        <a-button @click="test" type="text" size="mini" title="后一个事件">
                             <template #icon>
                                 <icon-to-right :style="{ fontSize: '18px' }" />
                             </template>
@@ -37,10 +47,16 @@
                         @mousedown.stop
                         :min="timeLine.min"
                         :max="timeLine.max"
-                        :step="100"
-                        show-ticks
+                        :step="1"
                         style="width: 100%;margin: 0 ;padding:0;margin-top: 100px;"
-                        :marks="{ [timeLine.min]: '', [timeLine.max]: '' }"
+                        :marks="timeLineMarks"
+                        :format-tooltip="(value: number) => {
+                            if (value === 0) {
+                                return '数学零点(无意义)';
+                            } else {
+                                return value + '年';
+                            }
+                        }"
                     />
                 </div>
             </div>
@@ -93,6 +109,15 @@ import {
 import { throttle } from '../utils/flowControl';
 import * as echarts from 'echarts';
 import '../style/fine-tune-timeLine.scss';// 局部组件库样式微调
+import { nextTick } from 'process';
+
+const test = () => {
+    // console.log(sliderBox.value.clientWidth);
+    // console.log(slider.value.clientWidth);
+    // console.log(sliderBox.value.onmousedown);
+    // console.log(timeLineMarks.value);
+    console.log(scaleFactor.value);
+}
 
 const tempData = reactive({
     data: [
@@ -125,16 +150,57 @@ const tempData = reactive({
         { id: 27, timeSlot: 2019, title: 'xxxxx', desc: 'По сути, стратегия позиционирования определяет рекламный макет. Маркетинговая активность отражает медиаплан, не считаясь с затратами. Общество потребления переворачивает ролевой поведенческий таргетинг.' },
         { id: 28, timeSlot: 2020, title: 'xxxxx', desc: 'По сути, стратегия позиционирования определяет рекламный макет. Маркетинговая активность отражает медиаплан, не считаясь с затратами. Общество потребления переворачивает ролевой поведенческий таргетинг.' }]
 })
-const timeLine = reactive({ min: -10000, max: 10000 });
+const timeLine = reactive({ min: -5000, max: 5000 });
 // 当前年份
 const currentYear = ref(0);
 // 当前年份占总时间轴的长度比例
 const percentYear = computed(() => {
     return (currentYear.value - timeLine.min) / (timeLine.max - timeLine.min);
 })
+// 时间轴和步长的缩放比例
+const scaleFactor = ref(1);
 // 时间轴的长度'px'
 const sliderWidth = computed(() => {
-    return Math.round((timeLine.max - timeLine.min) / 10);
+    return Math.round(scaleFactor.value * ((timeLine.max - timeLine.min) / 10));
+})
+// 时间轴刻度的步长
+// const stepValue = computed(() => {
+//     const timeLength = timeLine.max - timeLine.min;
+//     return Math.round(timeLength / 40);
+//     //5000->125 10000->250 20000->500...
+// })
+// 等分时间轴添加标度
+const timeLineMarks = computed(() => {
+    let division = 1;
+    if (scaleFactor.value < 4) {
+        division = 1;
+    } else if (scaleFactor.value < 6) {
+        division = 2;
+    } else if (scaleFactor.value < 8) {
+        division = 4;
+    } else if (scaleFactor.value < 10) {
+        division = 8;
+    } else {
+        division = 16;
+    }
+    const stepArr = [], stepObj = { [timeLine.min]: timeLine.min };
+    const timeLength = timeLine.max - timeLine.min;
+    let stepOne = Math.round(timeLength / 20 / division), step = timeLine.min;
+    while (1) {
+        if (step <= timeLine.max) {
+            stepArr.push(step);
+            step += stepOne;
+        } else {
+            break;
+        }
+    }
+    stepArr.forEach(item => {
+        stepObj[item] = item;
+    })
+    if (timeLine.min < 0) {
+        stepObj[0] = 0;
+    }
+    return stepObj;
 })
 
 /*
@@ -197,22 +263,9 @@ const slider = ref(), sliderBox = ref(), spiralChart = ref();
 onMounted(() => {
     calculateOffsetTop();
     setSpiralChart();
+    setSliderState();
     // 左右滑动时间轴
-    sliderBox.value.onmousedown = function (e: MouseEvent) {
-        const x = e.pageX - slider.value.offsetLeft;
-        sliderBox.value.onmousemove = fn;
-        document.onmouseup = function () {
-            sliderBox.value.onmousemove = null;
-        }
-        function fn(e: MouseEvent) {
-            // 时间轴宽度  - 时间轴视口宽度
-            const max = sliderWidth.value - sliderBox.value.offsetWidth;
-            let targetX = e.pageX - x;
-            targetX > 0 ? targetX = 0 : null;
-            targetX < -1 * max ? targetX = -1 * max : null;
-            slider.value.style.left = targetX + 'px';
-        }
-    }
+    sliderBox.value.onmousedown = slidingTimeline;
     sliderBox.value.addEventListener('mouseleave', () => {
         // 主动触发mouseup避免滑块越界
         const evt = new Event('mouseup', { "bubbles": true, "cancelable": true });
@@ -220,9 +273,15 @@ onMounted(() => {
     })
     // 窗口大小改变时自动适应时间轴右侧
     window.addEventListener('resize', () => {
-        const sliderLeft = parseInt(slider.value.style.left) || 0;
-        const distance = sliderBox.value.clientWidth - (sliderWidth.value + sliderLeft);
-        if (distance > 0) slider.value.style.left = sliderLeft + distance + 'px';
+        // const sliderLeft = parseInt(slider.value.style.left) || 0;
+        // const distance = sliderBox.value.clientWidth - (sliderWidth.value + sliderLeft);
+        // if (distance > 0) slider.value.style.left = sliderLeft + distance + 'px';
+        // if (slider.value.clientWidth < sliderBox.value.clientWidth) {
+        //     //     slider.value.style.left = '0px';
+        //     // } else {
+        //     slider.value.style.left = 0.5 * (sliderBox.value.clientWidth - slider.value.clientWidth) + 'px';
+        // }
+        setSliderState();
     })
 })
 
@@ -257,6 +316,33 @@ const sectionScroll = throttle(() => {
     }
     toNavCenter();
 }, 30);
+
+// 控制时间轴长度缩放
+const controlScaling = (type: string) => {
+    // 最大25倍
+    if (scaleFactor.value <= 25 && type === 'enlarge') {
+        scaleFactor.value += 1;
+    } else if (scaleFactor.value > 0.5 && type === 'reduce') {
+        scaleFactor.value -= 0.5;
+    }
+    setSliderState();
+}
+// 时间轴状态设置
+function setSliderState() {
+    nextTick(() => {
+        const boxWidth = sliderBox.value.clientWidth, sliderWidth = slider.value.clientWidth;
+        // 判断slider与视口的宽度并卸载/添加滑动事件
+        if (sliderWidth > boxWidth) {
+            // 获取滑块位置占比
+            const proportion = parseFloat((<HTMLElement>document.querySelector('.arco-slider-btn')).style.left);
+            if (sliderBox.value.onmousedown === null) sliderBox.value.onmousedown = slidingTimeline;
+            slider.value.style.left = proportion * (boxWidth - sliderWidth) / 100 + 'px';
+        } else {
+            if (sliderBox.value.onmousedown !== null) sliderBox.value.onmousedown = null;
+            slider.value.style.left = 0.5 * (boxWidth - sliderWidth) + 'px';
+        }
+    })
+}
 
 // 计算每个元素距离父元素顶部的大小
 function calculateOffsetTop() {
@@ -323,6 +409,22 @@ function setSpiralChart() {
         myChart!.resize();
     };
 }
+// 滑动时间轴
+function slidingTimeline(e: MouseEvent) {
+    const x = e.pageX - slider.value.offsetLeft;
+    sliderBox.value.onmousemove = fn;
+    document.onmouseup = function () {
+        sliderBox.value.onmousemove = null;
+    }
+    function fn(e: MouseEvent) {
+        // 时间轴宽度  - 时间轴视口宽度
+        const max = sliderWidth.value - sliderBox.value.offsetWidth;
+        let targetX = e.pageX - x;
+        targetX > 0 ? targetX = 0 : null;
+        targetX < -1 * max ? targetX = -1 * max : null;
+        slider.value.style.left = targetX + 'px';
+    }
+}
 </script>
 
 <style lang="scss" scoped>
@@ -349,7 +451,6 @@ function setSpiralChart() {
                 position: absolute;
                 top: 5px;
                 right: 50%;
-                // transform: translateX(-50%);
                 color: #165dff;
             }
             .slider-tools {
